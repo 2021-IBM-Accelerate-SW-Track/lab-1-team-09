cc6753840e00cc14e8c94b71ddc01bb1c9819baa
KyleYoung69 <84196879+KyleYoung69@users.noreply.github.com>
2021-06-20 12:20:59 -0700

    Title card
    made to fit TODO list idea better

--- src/component/Body/TodoList.js
+++ src/component/Body/TodoList.js
@@ -45,7 +45,7 @@ function TodoList() {
    
     return (
         <div>
-        <h1>What is the plan for today</h1>
+        <h1>What's TO DO today?</h1>
         <TodoForm onSubmit={addTodo}/>
         <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo}/>
         </div>
